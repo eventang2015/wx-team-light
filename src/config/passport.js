282850d08d8c16034c280ee2128cfbf3564a23b4
Even Tang <eventang2015@outlook.com>
2022-11-16 23:00:32 +0800

    fix

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const passportJWT = require('passport-jwt');
-const db = require("../server/models");
+const db = require("../models");
 const User = db.Users;
 const JWT_SECRET = process.env.JWT_SECRET || 'wx:miniprogram-team-light:passport';
 
